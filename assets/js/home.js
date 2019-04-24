4aff3b940b0cc00183bbf13c0fd5253902f4c3bc
Lars S. Laichter <lars.laichter@gmail.com>
2019-04-24 09:14:29 -0700

    added jQuery

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,4 @@
 $("menuItem").hover(function(event){
-	$("body").css("background-image", "url('http://ourfutures.co/wp-content/uploads/2018/11/46305277_309420783228705_7885087530528276480_n.jpg')");
+	$("body").css("background", "white");
+	window.alert("YES!")
 });
