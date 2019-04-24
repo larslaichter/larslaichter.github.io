00761bab994523d6f38c5515606e80d7f4378887
Lars S. Laichter <lars.laichter@gmail.com>
2019-04-23 23:56:50 -0700

    jQuery added

---
+++ assets/js/home.js
@@ -0,0 +1,3 @@
+$("menuItem").hover(function(event){
+	$("body").css("background-image", "url('http://ourfutures.co/wp-content/uploads/2018/11/46305277_309420783228705_7885087530528276480_n.jpg')");
+});
